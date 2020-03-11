f0b533387661758d63c0fd009b08b53d16ea023c
Zahir <zahir@zedestech.com>
2020-03-11 11:56:07 +0530

    [FIX] No Raise for status

--- mws/mws.py
+++ mws/mws.py
@@ -305,7 +305,7 @@ class MWS(object):
             # to convert the dict to a url parsed string, so why do it twice if i can just pass the full url :).
             response = request(method, url, data=kwargs.get(
                 'body', ''), headers=headers, proxies=proxies, timeout=kwargs.get('timeout', 300))
-            response.raise_for_status()
+            #response.raise_for_status()
             # When retrieving data from the response object,
             # be aware that response.content returns the content in bytes while response.text calls
             # response.content and converts it to unicode.
